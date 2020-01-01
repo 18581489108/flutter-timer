e91d8ba1c790153db9344fb6760b8198ca5dd10a
kurisu9 <18581489108@163.com>
2020-01-01 18:50:21 +0800

    添加倒计时的处理

--- lib/screens/countdownPage.dart
+++ lib/screens/countdownPage.dart
@@ -1,25 +1,87 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:timer/common/theme.dart';
+import 'package:timer/models/timerSetting.dart';
+import 'package:timer/models/globalData.dart';
 
 class CountdownPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CurrentTimerSettingModel currentTimerSettingModel =
+        Provider.of<CurrentTimerSettingModel>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Count Down', style: Theme.of(context).textTheme.display4),
         backgroundColor: Colors.white,
       ),
-      body: _CountdownContainer(),
+      body: _CountdownContainer(
+          timerSetting: TimerSetting.copy(currentTimerSettingModel.timerSetting)),
     );
   }
 }
 
 class _CountdownContainer extends StatefulWidget {
+  final TimerSetting timerSetting;
+
+  const _CountdownContainer({Key key, this.timerSetting}) : super(key: key);
+
   @override
   _CountdownContainerState createState() => _CountdownContainerState();
 }
 
 class _CountdownContainerState extends State<_CountdownContainer> {
+  Timer _countdownTimer;
+  int _countdownNum = 59;
+  TimerSetting _timerSetting;
+  int _singleTime;
+  int _intervalTime;
+  
+  /// 标记倒计时是单次时间还是间隔时间
+  /// true 为单次时间
+  /// false 为间隔时间
+  bool _countdownFlag;
+
+  /// 倒计时的间隔
+  static const int CONTDOWN_INTERVAL_MILL = 100;
+  @override
+  void initState() {
+    super.initState();
+
+    _timerSetting = widget.timerSetting;
+    _singleTime = _timerSetting.singleTime * 1000;
+    _countdownFlag = true;
+
+    _countdownTimer =
+        new Timer.periodic(new Duration(milliseconds: CONTDOWN_INTERVAL_MILL), (timer) {
+          setState(() {
+            if (_timerSetting.totalTimes <= 0) {
+              _countdownTimer.cancel();
+              _countdownTimer = null;
+              return;
+            }
+            if (_countdownFlag) {
+              if (_singleTime > 0) {
+                _singleTime -= CONTDOWN_INTERVAL_MILL;
+              } else {
+                _countdownFlag = !_countdownFlag;
+                _intervalTime = _timerSetting.singleInterval * 1000;
+              }
+            } else {
+              if (_intervalTime > 0) {
+                _intervalTime -= CONTDOWN_INTERVAL_MILL;
+              } else {
+                _countdownFlag = !_countdownFlag;
+                _singleTime = _timerSetting.singleTime * 1000;
+                _timerSetting.totalTimes--;
+              }
+            }
+            
+          });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -70,7 +132,7 @@ class _CountdownContainerState extends State<_CountdownContainer> {
                   value: .7,
                 ),
               ),
-              Text('剩余时间')
+              Text('剩余时间${_countdownFlag ? _singleTime : _intervalTime}'),
             ],
           ),
         ),
@@ -114,5 +176,13 @@ class _CountdownContainerState extends State<_CountdownContainer> {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _countdownTimer?.cancel();
+    _countdownTimer = null;
+    print('销毁 _CountdownContainerState');
+    super.dispose();
   }
 }
